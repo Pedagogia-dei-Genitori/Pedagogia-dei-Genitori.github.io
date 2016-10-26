839643afe53970d933655f24e147830df5a50c62
Pedagogia-dei-Genitori <Pedagogia-dei-Genitori@users.noreply.github.com>
2016-10-26 02:29:11 +0100

    Finished implementing page transitions, apart from handling back/forward.

--- js/pageTransitions.js
+++ js/pageTransitions.js
@@ -59,25 +59,54 @@ function pageIsReadyForTransitions()
 
   function loadNewPageContent(pageToLoad, addPageToHistory)
   {
-    // Set the correct class on the body to match which type of page we are loading (e.g. homepage or subpage) - used for CSS
-    if ($("body").hasClass("homepageBody"))
-    {
-      $("body").removeClass(); // Remove all classes
-      $("body").addClass("subpageBody");
-    }
-    else if ($("body").hasClass("subpageBody"))
-    {
-      $("body").removeClass(); // Remove all classes
-      $("body").addClass("homepageBody");
-      $("body").addClass("homepageAnimated");
-    }
-
     // Create a new content wrapper for the <main> element by loading it from the page we are transitioning to
     var newMainContentWrapper = $("<div class='mainContentWrapper'></div>");
     newMainContentWrapper.load(pageToLoad + " .mainContentWrapper > *", function(e)
     {
+      // Make sure the menu gets closed in case it's open
+      if ($(".menuBtn").hasClass("is-active"))
+        $(".menuBtn").click();
+
       // Set the html content of the <main> element to be the wrapper we created
       $("main").html(newMainContentWrapper);
+
+      // Set 2 second timeout, which allows animations to hide old page and show progress bar
+      setTimeout(function()
+      {
+        // This triggers the animation to expand the progress bar and show the new page
+        $("body").removeClass("pageTransitioning");
+
+        // Set the correct class on the body to match which type of page we are loading (e.g. homepage or subpage) - used for CSS
+        if ($("body").hasClass("homepageBody"))
+        {
+          $("body").removeClass(); // Remove all classes
+          $("body").addClass("subpageBody");
+        }
+        else if ($("body").hasClass("subpageBody"))
+        {
+          $("body").removeClass(); // Remove all classes
+          $("body").addClass("homepageBody");
+          $("body").addClass("homepageAnimated");
+        }
+
+        // Wait until for the first animation on the progress bar to finish
+        $(".pageTransitionLoadingBar").one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend", function()
+        {
+          $(".pageTransitionLoadingBar").off("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend");
+
+          // Were now free to transition to another page
+          pageIsCurrentlyTransitioning = false;
+        });
+      }, 2000);
+
+      // Add the page we've loaded to the window's history
+      if (addPageToHistory)
+      {
+        if (pageToLoad != window.location)
+        {
+          window.history.pushState({path: pageToLoad}, "", pageToLoad);
+        }
+      }
     });
   }
 }
